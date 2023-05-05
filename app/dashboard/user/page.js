db13555e21893b3f0c20cd73c69955c17c575db9
jebwilfred <jwdpanganiban@gmail.com>
2023-05-05 15:50:40 +0800

    Feat: Add list of owned lots

--- app/dashboard/user/page.js
+++ app/dashboard/user/page.js
@@ -1,11 +1,19 @@
 'use client';
 import DashboardLayout from '@/app/components/layouts/DashboardLayout';
 
+import LotCard from '@/app/components/dashboard/user/LotCard';
+
+import styles from '@/app/styles/dashboard/user/UserDashboard.module.css';
+
 const UserDashboard = () => {
     return (
         <>
             <DashboardLayout userType="user">
-                <span>Test</span>
+                <h2 className={styles['header']}>Owned Lots</h2>
+                <div className={styles['cards-container']}>
+                    <LotCard name="Remo, Rafael" block="1" section="A" lot="3"/>
+                    <LotCard name="Zarsuela, Ynigo" block="2" section="B" lot="5"/>
+                </div>
             </DashboardLayout>
         </>
     );
