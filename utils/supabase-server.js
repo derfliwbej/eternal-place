628210fa6242d80c0d0bdc2956c8d12405a2c867
jebwilfred <jwdpanganiban@gmail.com>
2023-05-12 15:22:07 +0800

    Feat: Create Supabase server and browser clients

---
+++ utils/supabase-server.js
@@ -0,0 +1,5 @@
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { cookies, headers } from "next/headers";
+import "server-only";
+
+export const createClient = () => createServerComponentSupabaseClient({ headers, cookies });
