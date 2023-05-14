98cf9278301bcff18c844fb383362bc14b9acf78
jebwilfred <jwdpanganiban@gmail.com>
2023-05-14 20:53:31 +0800

    Fix: Updated directory name

--- app/components/error/ErrorText.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-const ErrorText = ({ children }) => {
-    return (
-        <Typography color="error">{children}</Typography>
-    );
-};
-
-export default ErrorText;
