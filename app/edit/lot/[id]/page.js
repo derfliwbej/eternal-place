1514879f666117d96fceca86c39c14e0ffc6a6cf
jebwilfred <jwdpanganiban@gmail.com>
2023-05-17 01:10:42 +0800

    Feat: Fetch data from database

--- app/edit/lot/[id]/page.js
+++ app/edit/lot/[id]/page.js
@@ -8,7 +8,7 @@ import LotTomb from '@/app/components/edit/lot/LotTomb';
 import LotOwners from '@/app/components/edit/lot/LotOwners';
 import LotImage from '@/app/components/LotImage';
 
-import { Divider, Button } from '@mui/material';
+import { Divider, Button, CircularProgress } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
 const ViewLotPage = ({ params }) => {
@@ -17,9 +17,9 @@ const ViewLotPage = ({ params }) => {
     const [lot, setLot] = useState({});
     const [owners, setOwners] = useState([]);
     const [tombs, setTombs] = useState([]);
+    const [initialLoad, setInitialLoad] = useState(false);
 
     const handleAddTomb = () => {
-        // TODO: API Call for adding tomb then getting the id from the backend
         setTombs(current => [...current, { id: 0, deceased_list: [] }]);
     };
 
@@ -28,42 +28,71 @@ const ViewLotPage = ({ params }) => {
         setTombs(newTombs);
     };
 
+    useEffect( () => {
+        const fetchLot = async () => {
+            setInitialLoad(true);
+            const res = await fetchUtil(`/lot?id=${id}`);
+
+            const data = await res.json();
+
+            setLot(data.lot);
+            setOwners(data.owners);
+            setTombs(data.tombs);
+
+            setInitialLoad(false);
+        };
+
+        fetchLot();
+    }, []);
+
     return (
         <DashboardLayout userType="admin">
-            <h2>Block 1, Section A, Lot 3</h2>
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
-            <div>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    {/* TODO: Image view and upload */}
-                    <LotImage src="/pantheon.jpg" alt="Lot Image" size={500} />
-                </div>
-            </div>
+            <>
+                {
+                    !initialLoad ? (
+                        <>
+                            <h2>Block {lot.block}, Section {lot.section}, Lot {lot.lot_num}</h2>
+                            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                            <div>
+                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                    {/* TODO: Image view and upload */}
+                                    <LotImage src="/pantheon.jpg" alt="Lot Image" size={500} />
+                                </div>
+                            </div>
 
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
 
-            <LotOwners owners={owners} />
+                            <LotOwners owners={owners} />
 
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
 
-            <div style={{ display: 'flex '}}>
-                <h2>Lot Tombs</h2>
-                <Button sx={{ marginLeft: 3 }} variant="contained" startIcon={<AddIcon />} onClick={handleAddTomb}>
-                    Create New Tomb
-                </Button>
-            </div>
-            
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                            <div style={{ display: 'flex '}}>
+                                <h2>Lot Tombs</h2>
+                                <Button sx={{ marginLeft: 3 }} variant="contained" startIcon={<AddIcon />} onClick={handleAddTomb}>
+                                    Create New Tomb
+                                </Button>
+                            </div>
+                            
+                            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
 
-            {tombs.map( (tomb, i) => {
-                return (
-                    <div key={i}>
-                        <LotTomb id={tomb.id} deceasedList={tomb.deceased_list} deleteTomb={deleteTomb} />
-                        <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
-                    </div>
-                );
-            })}
+                            {tombs.map( (tomb, i) => {
+                                return (
+                                    <div key={i}>
+                                        <LotTomb id={tomb.id} deceasedList={tomb.deceased_list} deleteTomb={deleteTomb} />
+                                        <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+                                    </div>
+                                );
+                            })}
 
-            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                            <Divider sx={{ marginTop: 2, marginBottom: 2 }}/>
+                        </>
+                    ) : (
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <CircularProgress />
+                        </div>
+                    )
+                }
+            </>
         </DashboardLayout>
     );
 };
