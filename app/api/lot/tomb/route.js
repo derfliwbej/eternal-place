f185b8ecf08368fbabdb957e954e69cd5b27b569
jebwilfred <jwdpanganiban@gmail.com>
2023-05-17 20:14:29 +0800

    Feat: Add route handler for deleting tomb

--- app/api/lot/tomb/route.js
+++ app/api/lot/tomb/route.js
@@ -24,3 +24,25 @@ export async function POST(request) {
 
     return NextResponse.json({ ...tomb, deceased_list: [] });
 }
+
+export async function DELETE(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    const { error } = await supabase
+        .from('tombs')
+        .delete()
+        .eq('id', id);
+
+    if (error) return new Response("Internal Server Error", { status: 500 });
+
+    return new Response("Successfully deleted", { status: 200 });
+}
