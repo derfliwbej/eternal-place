ca3f73c9b426c1535451f68d5b3db4c8b2a3c25a
jebwilfred <jwdpanganiban@gmail.com>
2023-05-18 02:25:04 +0800

    Feat: Add route handler for updating lot image path

--- app/api/lot/route.js
+++ app/api/lot/route.js
@@ -40,3 +40,46 @@ export async function GET(request) {
 
     return NextResponse.json({ lot, owners, tombs });
 }
+
+export async function PUT(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    const { image_path } = await request.json();
+
+    const { data: { image_path: previous_path }, errorPrevious } = await supabase
+        .from('lots')
+        .select('image_path')
+        .eq('id', id)
+        .single();
+
+    if (errorPrevious) return new Response(error.message, { status: 500 });
+
+    const { data: lot, errorNew } = await supabase
+        .from('lots')
+        .update({ image_path })
+        .eq('id', id)
+        .select()
+        .single();
+
+    if (errorNew) return new Response(error.message, { status: 500 });
+
+    if (previous_path) {
+        const { _, errorRemove } = await supabase
+            .storage
+            .from('lot_images')
+            .remove([previous_path]);
+
+        if (errorRemove) return new Response(error.message, { status: 500 });
+    }
+
+    return NextResponse.json(lot);
+}
