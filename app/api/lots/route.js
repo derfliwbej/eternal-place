b09427bb49944722b062cc94e21744e04234c4ac
jebwilfred <jwdpanganiban@gmail.com>
2023-05-18 21:24:23 +0800

    Feat: Add route handler for resetting all lightings of lots

--- app/api/lots/route.js
+++ app/api/lots/route.js
@@ -18,3 +18,29 @@ export async function GET(request) {
 
     return NextResponse.json(lots);
 }
+
+export async function PUT(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { errorReset } = await supabase.rpc('reset_lot_lightings');
+
+    if (errorReset) {
+        console.log(errorReset);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+
+    const { data: lots, errorLots } = await supabase.rpc('get_lots_map');
+
+    if (errorLots) {
+        console.log(errorLots);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+
+    return NextResponse.json(lots);
+}
