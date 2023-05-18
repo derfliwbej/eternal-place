4f5a65cb624728d1ded835bfbf5c111ca5c655d4
jebwilfred <jwdpanganiban@gmail.com>
2023-05-18 16:49:40 +0800

    Feat: Create route handler for setting lot light status

---
+++ app/api/lot/light/route.js
@@ -0,0 +1,26 @@
+import { createClient } from "@/utils/supabase-server";
+import { NextResponse } from "next/server";
+
+export async function PUT(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get('id');
+
+    const { has_light } = await request.json();
+
+    const { error } = await supabase
+        .from('lots')
+        .update({ has_light })
+        .eq('id', id);
+
+    if (error) return new Response("Internal Server Error", { status: 500 });
+
+    return new Response("Successfully updated", { status: 200 });
+}
