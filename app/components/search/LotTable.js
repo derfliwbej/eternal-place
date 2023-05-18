f9c0d239d9bf4a3af59d3ffb20ed68a1397cb313
jebwilfred <jwdpanganiban@gmail.com>
2023-05-19 00:07:16 +0800

    Feat: Get list of lots from backend and render it

--- app/components/search/LotTable.js
+++ app/components/search/LotTable.js
@@ -5,31 +5,28 @@ import { Button } from '@mui/material';
 
 import { useRouter } from 'next/navigation';
 
-const initialRows = [
-    { id: 1, block: 1, section: 'A', lot: 1, isMausoleum: true, deceasedNames: 'Panganiban, Jeb', owners: 'Sales, Ella' }
-];
-
-const LotTable = ({ rowsProps }) => {
+const LotTable = ({ rows, loading }) => {
     const router = useRouter();
-
-    const [rows, setRows] = useState(rowsProps || initialRows);
 
     const handleRedirect = (id) => {
         router.push(`/edit/lot/${id}`);
     };
 
     const columns = [
-        { field: 'id', headerName: 'ID', type: 'number', flex: 0.3 },
-        { field: 'block', headerName: 'Block', type: 'number', flex: 0.3 },
-        { field: 'section', headerName: 'Section', type: 'string', flex: 0.3 },
-        { field: 'lot', headerName: 'Lot', type: 'number', flex: 0.3 },
-        { field: 'isMausoleum', headerName: 'Is Mausoleum', type: 'boolean', flex: 0.3 },
-        { field: 'deceasedNames', headerName: 'Deceased Name(s)', type: 'string', flex: 1 },
-        { field: 'owners', headerName: 'Owners', type: 'string', flex: 1 },
+        { field: 'id', headerName: 'ID', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'block', headerName: 'Block', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'section', headerName: 'Section', type: 'string', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'lot_num', headerName: 'Lot', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'is_mausoleum', headerName: 'Is Mausoleum', type: 'boolean', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'has_light', headerName: 'Has Light', type: 'boolean', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'lot_deceased', headerName: 'Deceased Name(s)', type: 'string', align: 'center', headerAlign: 'center', flex: 1 },
+        { field: 'lot_owners', headerName: 'Owners', type: 'string', align: 'center', headerAlign: 'center', flex: 1 },
         { 
             field: 'view', 
             headerName: 'Link', 
             flex: 0.3,
+            align: 'center',
+            headerAlign: 'center',
             renderCell: (params) => (
                 <Button onClick={ (event) => handleRedirect(params.row.id) }>View Lot</Button>
             ) 
@@ -56,7 +53,13 @@ const LotTable = ({ rowsProps }) => {
                         },
                       }}
                       pageSizeOptions={[10, 15, 20, 25, 30]}
-                      
+                      loading={loading}
+                      sx={{
+                        "& .MuiDataGrid-cell": {
+                            whiteSpace: "normal !important",
+                            worwdWrap: "break-word !important"
+                        }
+                      }}
             />
         </div>
     );
--- app/search/page.js
+++ app/search/page.js
@@ -1,12 +1,47 @@
+'use client';
+import { useState, useEffect } from 'react';
 import DashboardLayout from "../components/layouts/DashboardLayout";
+import fetchUtil from '@/utils/fetchUtil';
 
 import LotTable from "../components/search/LotTable";
+import { CircularProgress } from '@mui/material';
+import ErrorDialog from '@/app/components/prompt/ErrorDialog';
 
 const LotSearchPage = () => {
+    const [loading, setLoading] = useState(false);
+    const [rows, setRows] = useState([]);
+    const [errorDialog, setErrorDialog] = useState({});
+    const [showError, setShowError] = useState(false);
+
+    useEffect( () => {
+        const fetchLots = async () => {
+            try {
+                setLoading(true);
+
+                const res = await fetchUtil('/lots_table');
+                const lots = await res.json();
+
+                setRows(lots);
+            } catch(error) {
+                setErrorDialog({ title: 'Error', message: error.message });
+                setShowError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchLots();
+    }, []);
+
     return (
         <DashboardLayout userType="admin">
-            <h2 style={{ marginBottom: 20 }}>Viewing All Lots</h2>
-            <LotTable />
+            {loading ? <CircularProgress /> : (
+                <>
+                    <h2 style={{ marginBottom: 20 }}>Viewing All Lots</h2>
+                    <LotTable rows={rows} loading={loading} />
+                </>
+            )}
+            <ErrorDialog title={errorDialog.title} message={errorDialog.message} open={showError} setOpen={setShowError} />
         </DashboardLayout>
     );
 };
