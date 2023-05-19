0792c8369c594780128ca23311d59c4e5de54a93
jebwilfred <jwdpanganiban@gmail.com>
2023-05-19 20:10:37 +0800

    Feat: Create route handler for getting user's owned lots

---
+++ app/api/owned_lots/route.js
@@ -0,0 +1,23 @@
+import { createClient } from "@/utils/supabase-server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const id = session.user.id;
+
+    const { data: lots, error } = await supabase.rpc('get_owned_lots', { uid: id });
+
+    if (error) {
+        console.log(error);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+
+    return NextResponse.json(lots);
+}
