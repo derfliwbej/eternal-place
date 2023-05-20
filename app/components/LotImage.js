8be6c71f486eb4497c06e34ace586428e4899989
jebwilfred <jwdpanganiban@gmail.com>
2023-05-21 01:27:50 +0800

    Refactor: Changed url path

--- app/components/LotImage.js
+++ app/components/LotImage.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const LotImage = ({ src, alt, size }) => {
+const LotImage = ({ path, alt, size }) => {
     const [ratio, setRatio] = useState(16 / 9);
 
     return (
-        <Image src={src} alt={alt} width={size} height={size / ratio} fixed="true" onLoadingComplete={({ naturalWidth, naturalHeight }) => {
+        <Image src={path ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/lot_images/${path}` : '/placeholder.png'} alt={alt} width={size} height={size / ratio} fixed="true" onLoadingComplete={({ naturalWidth, naturalHeight }) => {
             setRatio(naturalWidth / naturalHeight);
         }} />
     );
