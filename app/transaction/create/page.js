ca708ec66cf877ce1fb9d20edbe98117c5922344
jebwilfred <jwdpanganiban@gmail.com>
2023-05-21 23:03:33 +0800

    Feat: Get available lots for lighting from backend

--- app/transaction/create/page.js
+++ app/transaction/create/page.js
@@ -1,53 +1,113 @@
 'use client';
+import { useState, useEffect } from 'react';
 import DashboardLayout from '@/app/components/layouts/DashboardLayout';
+import fetchUtil from '@/utils/fetchUtil';
 
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, CircularProgress } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import ErrorDialog from '@/app/components/prompt/ErrorDialog';
 
 const columns = [
-    { field: 'id', headerName: 'Lot ID', flex: 1 },
-    { field: 'block', headerName: 'Block Number', flex: 1 },
-    { field: 'section', headerName: 'Section', flex: 1 },
-    { field: 'lot', headerName: 'Lot Number', flex: 1 },
-    { field: 'deceased', headerName: 'Deceased Name(s)', flex: 7 }
-];
-
-const rows = [
-    { id: 1, block: 1, section: 'A', lot: 1, deceased: "Rafael Remo, Ynigo Zarsuela" }
+    { field: 'id', headerName: 'ID', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'block', headerName: 'Block', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'section', headerName: 'Section', type: 'string', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'lot_num', headerName: 'Lot', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'is_mausoleum', headerName: 'Is Mausoleum', type: 'boolean', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'tomb_count', headerName: 'Number of Tombs', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 }
 ];
 
 const Toolbar = ({ amount }) => {
     return (
-        <Typography sx={{ padding: 2 }}variant="h6" component="div">Amount to pay: {amount}</Typography>
+        <Typography sx={{ padding: 2 }}variant="h6" component="div">{`Amount to pay: PHP${amount}.00`}</Typography>
     );
 };
 
 const MakeTransactionPage = () => {
-    const amount = 0;
+
+    const [rows, setRows] = useState([]);
+    const [amount, setAmount] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [errorDialog, setErrorDialog] = useState({ title: '', message: '' });
+    const [showError, setShowError] = useState(false);
+    const [selectionModel, setSelectionModel] = useState([]);
+
+    const onRowSelectionModelChange = selection => {
+        const amountPerTomb = 500;
+        if (selection.length >= 1) {
+            const selectionSet = new Set(selectionModel);
+            const result = selection.filter( s => !selectionSet.has(s));
+            
+            const rowID = result[0];
+            const selectedRow = rows.find( currentRow => currentRow.id == rowID);
+
+            setAmount(amountPerTomb * selectedRow?.tomb_count);
+
+            setSelectionModel(result);
+        } else {
+            setAmount(0);
+            setSelectionModel(selection);
+        }
+    };
+
+    useEffect( () => {
+        const makeRequest = async () => {
+            try {
+                setLoading(true);
+                const res = await fetchUtil('/user/lotsForLighting');
+                const lots = await res.json();
+
+                setRows(lots);
+            } catch(error) {
+                setErrorDialog({ title: 'Error', message: error.message });
+                setShowError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        makeRequest();
+    }, []);
 
     return (
         <DashboardLayout userType="user">
-            <h3 style={{ marginBottom: 10 }}>Select all lots for lighting</h3>
-            <DataGrid rows={rows}
-                      columns={columns}
-                      initialState={{
-                        pagination: {
-                            paginationModel: {
-                                pageSize: 5,
-                            },
-                        },
-                      }}
-                      slots={{
-                        toolbar: Toolbar
-                      }}
-                      slotProps={{
-                        toolbar: { amount }
-                      }}
-                      pageSizeoptions={[5]}
-                      checkboxSelection
-                      disableRowSelectionOnClick
-            />
-            <Button sx={{ marginTop: 3 }} variant="contained">Submit</Button>
+            {loading ? (
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                    <CircularProgress />
+                </div>
+            ) : (
+                <>
+                    <h3 style={{ marginBottom: 10 }}>Available lots for lighting</h3>
+                    <DataGrid rows={rows}
+                            columns={columns}
+                            initialState={{
+                                pagination: {
+                                    paginationModel: {
+                                        pageSize: 5,
+                                    },
+                                },
+                            }}
+                            slots={{
+                                toolbar: Toolbar
+                            }}
+                            slotProps={{
+                                toolbar: { amount }
+                            }}
+                            pageSizeoptions={[5, 10, 15, 20]}
+                            checkboxSelection
+                            disableRowSelectionOnClick
+                            hideFooterSelectedRowCount
+                            rowSelectionModel={selectionModel}
+                            onRowSelectionModelChange={onRowSelectionModelChange}
+                            sx={{
+                                '& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer': {
+                                    display: 'none'
+                                }
+                            }}
+                    />
+                    <Button disabled={!selectionModel.length} sx={{ marginTop: 3 }} variant="contained">Proceed to Payment</Button>
+                </>
+            )}
+            <ErrorDialog title={errorDialog.title} message={errorDialog.message} open={showError} setOpen={setShowError} />
         </DashboardLayout>
     );
 };
