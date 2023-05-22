40eaa26544a034488af42819ff26c8add54778f1
jebwilfred <jwdpanganiban@gmail.com>
2023-05-22 21:32:28 +0800

    Feat: Add transaction history feature

---
+++ app/api/user/transactionHistory/route.js
@@ -0,0 +1,26 @@
+import { createClient } from "@/utils/supabase-server";
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+    const supabase = createClient();
+
+    const { data: { session }, } = await supabase.auth.getSession();
+
+    if(!session) {
+        return new Response("Unauthorized", { status: 401 });
+    }
+
+    const userID = session.user.id;
+
+    const { data: transactions, error } = await supabase
+        .from('transaction')
+        .select('*')
+        .eq('user_id', userID);
+
+    if (error) {
+        console.log(error);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+
+    return NextResponse.json(transactions);
+}
--- app/transaction/history/page.js
+++ app/transaction/history/page.js
@@ -1,52 +1,86 @@
 'use client';
 import DashboardLayout from "@/app/components/layouts/DashboardLayout";
+import { useState, useEffect } from 'react';
 
-import { Table, TableBody, TableCell,
-         TableHead, TableRow } from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { CircularProgress } from "@mui/material";
+import fetchUtil from "@/utils/fetchUtil";
+import ErrorDialog from "@/app/components/prompt/ErrorDialog";
+import dayjs from "dayjs";
 
-const createData = (transactionID, lotIDs, dateCreated, amount, referenceNumber, status) => {
-    return { transactionID, lotIDs, dateCreated, amount, referenceNumber, status };
-};
-
-const rows = [
-    createData(1, [1, 2], '2023-4-28', 600, 6750989, 'Paid')
-];
-
-const combineLotIDs = (lotIDs) => {
-    return lotIDs.reduce( (acc, current, i) => {
-        if(i == 0) return current
-        else return acc + `, ${current}`
-    }, '');
+const LoadingPage = () => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '80vh' }}>
+            <CircularProgress />
+        </div>
+    );
 };
 
 const TransactionHistoryPage = () => {
+
+    const [rows, setRows] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorDialog, setErrorDialog] = useState({ title: '', message: '' });
+    const [showError, setShowError] = useState(false);
+
+    const columns = [
+        { field: 'id', headerName: 'ID', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'lot_id', headerName: 'Lot ID', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'reference_num', headerName: 'Reference Number', type: 'text', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'date_created', headerName: 'Date Created', type: 'date', align: 'center', headerAlign: 'center', flex: 0.3,
+          valueFormatter: (params) => dayjs(params.value).format('YYYY-MM-DD') 
+        },
+        { field: 'amount', headerName: 'Amount', type: 'number', align: 'center', headerAlign: 'center', flex: 0.3 },
+        { field: 'status', headerName: 'Status', type: 'text', align: 'center', headerAlign: 'center', flex: 0.3 },
+    ];
+
+    useEffect( () => {
+        const makeRequest = async () => {
+            try {
+                setLoading(true);
+
+                const res = await fetchUtil('/user/transactionHistory');
+                const transactions = await res.json();
+
+                setRows(transactions);
+            } catch (error) {
+                setErrorDialog({ title: 'Error', message: error.message });
+                setShowError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        makeRequest();
+    }, []);
+
     return (
         <DashboardLayout userType="user">
-            <h3>Transaction History</h3>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Transaction ID</TableCell>
-                        <TableCell>Lot IDs</TableCell>
-                        <TableCell>Date Created</TableCell>
-                        <TableCell>Amount</TableCell>
-                        <TableCell>Reference Number</TableCell>
-                        <TableCell>Status</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map( (row) => (
-                        <TableRow key={row.transactionID}>
-                            <TableCell>{row.transactionID}</TableCell>
-                            <TableCell>{combineLotIDs(row.lotIDs)}</TableCell>
-                            <TableCell>{row.dateCreated}</TableCell>
-                            <TableCell>{row.amount}</TableCell>
-                            <TableCell>{row.referenceNumber}</TableCell>
-                            <TableCell>{row.status}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            {loading ? <LoadingPage /> : (
+                <>
+                    <h2 style={{ marginBottom: 5 }}>Transaction History</h2>
+                    <DataGrid rows={rows}
+                            columns={columns}
+                            slots={{ toolbar: GridToolbar }}
+                            slotProps={{
+                                toolbar: {
+                                    showQuickFilter: true,
+                                    printOptions: { disableToolbarButton: true },
+                                    csvOptions: { disableToolbarButton: true }
+                                }
+                            }}
+                            initialState={{
+                                pagination: {
+                                    paginationModel: {
+                                        pageSize: 10,
+                                    },
+                                },
+                            }}
+                            pageSizeOptions={[10, 15, 20, 25, 30]}
+                    />
+                </>
+            )}
+            <ErrorDialog title={errorDialog.title} message={errorDialog.message} open={showError} setOpen={setShowError} />
         </DashboardLayout>
     );
 };
