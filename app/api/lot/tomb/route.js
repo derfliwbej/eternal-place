0b1f11e1724bedd803c4e780081d569459454d3d
jebwilfred <jwdpanganiban@gmail.com>
2023-05-29 19:18:12 +0800

    Fix: Set lot's lighting status to false if last tomb is deleted

--- app/api/lot/tomb/route.js
+++ app/api/lot/tomb/route.js
@@ -35,26 +35,38 @@ export async function DELETE(request) {
     const { searchParams } = new URL(request.url);
     const id = searchParams.get('id');
 
-    const { error } = await supabase
+    const { data: tomb, error1 } = await supabase
+        .from('tombs')
+        .select('*')
+        .eq('id', id)
+        .select()
+        .single();
+
+    const lotID = tomb.lot_id;
+
+    if (error1) return new Response("Internal Server Error", { status: 500 });
+
+    const { error2 } = await supabase
         .from('tombs')
         .delete()
         .eq('id', id);
 
-    if (error) return new Response("Internal Server Error", { status: 500 });
-
-    const { data: tombs, error2 } = await supabase
-        .from('tombs')
-        .select('lot_id')
-        .eq('lot_id', id);
-
     if (error2) return new Response("Internal Server Error", { status: 500 });
 
-    if(tombs.length === 0) {
-        const { error3 } = await supabase
-            .from('lots')
-            .update({ has_light: false });
+    const { data: tombs, error3 } = await supabase
+        .from('tombs')
+        .select('lot_id')
+        .eq('lot_id', lotID);
 
-        if (error3) return new Response("Internal Server Error", { status: 500 });
+    if (error3) return new Response("Internal Server Error", { status: 500 });
+
+    if(tombs.length === 0) {
+        const { error4 } = await supabase
+            .from('lots')
+            .update({ has_light: false })
+            .eq('id', lotID);
+
+        if (error4) return new Response("Internal Server Error", { status: 500 });
     }
 
     return new Response("Successfully deleted", { status: 200 });
