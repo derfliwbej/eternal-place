3532cff67ec2336712d1e1e6f85746fdd4adf98e
jebwilfred <jwdpanganiban@gmail.com>
2023-06-14 20:07:33 +0800

    Feat: Add page for unknown pages

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+
+    return (
+        <div style={{ height: '100%', display: 'flex', backgroundColor: '#0E2745', justifyContent: 'center', alignItems: 'center' }}>
+            <p style={{ color: 'white' }}>The page you are trying to access does not exist or you have no permission to view it.</p>
+        </div>
+    );
+}
